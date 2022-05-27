cf24d241050d39075447ec3aa5b7c9fa6f0a9afc
Deb Kumar <59546212+MegaPanchamZ@users.noreply.github.com>
2022-05-27 15:01:43 +1000

    Doors

--- Assets/Interactivity/ClickToAnim.cs
+++ Assets/Interactivity/ClickToAnim.cs
@@ -1,53 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
-public class ClickToAnim : MonoBehaviour {
+public class ClickToAnim : MonoBehaviour
+{
+    public Animator animator;
+    public string lookAtTriggerName = "LookAt";
+    public string lookAwayTriggerName = "LookAway";
+    public string lookingAtBoolName = "LookingAt";
 
-	public Animator animator;
-	public string lookAtTriggerName = "LookAt";
-	public string lookAwayTriggerName = "LookAway";
-	public string lookingAtBoolName = "LookingAt";
+    public string mouseDownTriggerName = "MouseDown";
+    public string mouseUpTriggerName = "MouseUp";
+    public string mouseDragBoolName = "MouseDrag";
+    private bool isMouseOver = false;
+    private bool isMouseDrag = false;
 
-	public string mouseDownTriggerName = "MouseDown";
-	public string mouseUpTriggerName = "MouseUp";
-	public string mouseDragBoolName = "MouseDrag";
-	private bool isMouseOver = false;
-	private bool isMouseDrag = false;
-	private Camera camera;
+    void Update()
+    {
 
-	void Update () {
+        RaycastHit hitInfo;
 
-		 RaycastHit hitInfo;
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        if (Physics.Raycast(ray, out hitInfo) && hitInfo.collider.gameObject == gameObject)
+        {
+            if (!isMouseOver)
+            {
+                animator.SetTrigger(lookAtTriggerName);
+                isMouseOver = true;
+            }
 
-		 Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
- 		if (Physics.Raycast(ray, out hitInfo) && hitInfo.collider.gameObject == gameObject) {
- 			if (!isMouseOver) {
-				animator.SetTrigger(lookAtTriggerName);
-	 			isMouseOver = true;
- 			}
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                animator.SetTrigger(mouseDownTriggerName);
+                isMouseDrag = true;
+            }
 
- 			if (Input.GetMouseButtonDown(0)) {
-				animator.SetTrigger(mouseDownTriggerName);
-				isMouseDrag = true;
- 			}
+        }
+        else
+        {
+            if (isMouseOver)
+            {
+                animator.SetTrigger(lookAwayTriggerName);
+                isMouseOver = false;
+            }
+        }
 
+        if (Mouse.current.leftButton.wasPressedThisFrame && isMouseDrag)
+        {
+            animator.SetTrigger(mouseUpTriggerName);
+            isMouseDrag = false;
+        }
 
- 		}
- 		else {
- 			if (isMouseOver) {
-				animator.SetTrigger(lookAwayTriggerName);
-	 			isMouseOver = false;
- 			}
- 		}
+        animator.SetBool(lookingAtBoolName, isMouseOver);
+        animator.SetBool(mouseDragBoolName, isMouseDrag);
 
-		if (Input.GetMouseButtonUp(0) && isMouseDrag) {
-			animator.SetTrigger(mouseUpTriggerName);
-			isMouseDrag = false;
- 		}
-
-		animator.SetBool(lookingAtBoolName, isMouseOver);
-		animator.SetBool(mouseDragBoolName, isMouseDrag);
-
-	}
+    }
 
 }
